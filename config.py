f1f8ec5ff209de3306ff56d156c8fe8b97f5fec1
Logan Moore <lmooore@gmail.com>
2017-11-17 19:25:13 -0800

    Adding config file to tracked files

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    "author": "Logan Moore",
+    "footer": "",
+    "indexdesc": "Nothing a nap can't fix.",
+    "sidebardesc": "Nothing a nap can't fix.",
+    "title": "ThoseGrapefruits",
+    "headerpages": ["About"],
+    "year": "2017",
+}
